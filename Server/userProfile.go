61e65297f2c9c37e67a31acd7681506068c19654
StephenNnamani <sir.stephennnamani@gmail.com>
2022-06-05 12:41:26 +0100

    Continuous Build

---
+++ Server/userProfile.go
@@ -0,0 +1,68 @@
+import "fmt"
+
+// User represents data about a every new user.
+type User struct {
+	ID          string `json:"id"`
+	email       string `json:"email"`
+	password    string `json:"password"`
+	firstName   string `json:"firstName"`
+	lastName    string `json:"lastName"`
+	dateOfBirth string `json:"dateOfBirth"`
+	country     string `json:"country"`
+	state       string `json:"state"`
+	dPic        string `json:"dPic"`
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
